da16061201bbf23c136b7ef1aac75459f0b26418
Vladislav Kuznetsov <vlad.kuznetsov.97@gmail.com>
2019-08-21 12:04:53 +0300

    Take account of user points when computer shoosing cards

--- BlackJack/Computer.cs
+++ BlackJack/Computer.cs
@@ -21,29 +21,18 @@ namespace BlackJack
             Console.WriteLine($"Comp card {Hand} Sum ({TotalPoints})");
             return usedCards;
         }
-        public int GetCard(Card[] deck, int usedCards)
+        public int GetCard(Card[] deck, int usedCards, Player player)
         {
             for (int i = usedCards; i < deck.Length; i++)
             {
-
-                if (TotalPoints < 16)
+                if (player.TotalPoints < 21 || TotalPoints < 16)
                 {
                     TotalPoints += deck[i].Points;
                     usedCards++;
                     Hand += $"{deck[i].Name} {deck[i].Suit} ";
                     Console.WriteLine($"Comp card {Hand} Sum ({TotalPoints})");
-                    if (TotalPoints == 21)
-                    {
-                        Console.WriteLine("comp выиграл");
-                        break;
-                    }
-                    else if (TotalPoints > 21)
-                    {
-                        Console.WriteLine("Комп перебрал");
-                        break;
-                    }
                 }
-
+                
             }
             return usedCards;
         }
